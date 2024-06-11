8d11e5e599d907bd7bcc340ec91a0c193228d916
tembbo <m@tembbo.com>
2024-06-11 21:04:59 +0200

    feat(nvim): add CSS formatter

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -4,6 +4,7 @@ return {
 	config = function()
 		require("conform").setup({
 			formatters_by_ft = {
+				css = { "prettier" },
 				go = { "gofumpt" },
 				html = { "prettier" },
 				javascript = { "prettier" },
