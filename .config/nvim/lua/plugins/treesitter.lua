514f0571304a247ee779542293614988a61b9c02
tembbo <m@tembbo.com>
2024-06-11 21:28:51 +0200

    style(nvim): sort specs in treesitter.lua

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
 	event = "BufReadPost",
+	build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter.configs").setup({
 			auto_install = true,
