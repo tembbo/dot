1530713f4996711ffb7e7b0863057789aca760e0
tembbo <m@tembbo.com>
2024-07-25 22:10:03 +0200

    feat: disable change detection notification

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,4 +12,8 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("options")
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+    change_detection = {
+        notify = false,
+    },
+})
