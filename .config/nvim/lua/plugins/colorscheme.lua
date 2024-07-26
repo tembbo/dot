08e20cb534d66be5dc0b81dc523adac7edc9ac8e
tembbo <m@tembbo.com>
2024-07-26 18:04:19 +0200

    chore(nvim): use `init` instead of `config`

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,5 @@
 return {
     "catppuccin/nvim",
     name = "catppuccin",
-    priority = 1000,
-    config = function() vim.cmd("colorscheme catppuccin") end,
+    init = function() vim.cmd.colorscheme("catppuccin") end,
 }
