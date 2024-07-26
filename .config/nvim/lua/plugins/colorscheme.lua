8cfd495c864a015218c1a31f2a63b9e62bff7915
tembbo <m@tembbo.com>
2024-07-26 13:50:57 +0200

    style: collapse simple statements

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,7 +2,5 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    config = function()
-        vim.cmd("colorscheme catppuccin")
-    end,
+    config = function() vim.cmd("colorscheme catppuccin") end,
 }
