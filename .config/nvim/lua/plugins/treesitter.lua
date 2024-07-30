218da400631808a648feb4b8fdf50ba13bed9bd6
tembbo <m@tembbo.com>
2024-07-30 23:54:35 +0200

    chore: update treesitter config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,11 +2,10 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     event = "VeryLazy",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end,
+    opts = {
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+    },
+    config = function(opts) require("nvim-treesitter.configs").setup(opts) end,
 }
