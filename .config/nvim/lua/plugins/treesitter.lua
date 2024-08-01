b2e3ce0616214ede34e95075bffde964c9f25a34
tembbo <m@tembbo.com>
2024-08-01 17:09:22 +0200

    fix: use `main` spec instead of `config`

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,10 +2,10 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     event = "VeryLazy",
+    main = "nvim-treesitter.configs",
     opts = {
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
     },
-    config = function(opts) require("nvim-treesitter.configs").setup(opts) end,
 }
