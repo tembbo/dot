80b28f024ed43f805b952215528b47a103c3a39d
tembbo <m@tembbo.com>
2024-08-03 19:25:07 +0200

    style: remove extra square brackets

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -18,14 +18,12 @@ return {
         "neovim/nvim-lspconfig",
         event = { "BufReadPre", "BufNewFile" },
         dependencies = {
-            {
-                "williamboman/mason-lspconfig.nvim",
-                config = function()
-                    require("mason-lspconfig").setup({
-                        automatic_installation = true,
-                    })
-                end,
-            },
+            "williamboman/mason-lspconfig.nvim",
+            config = function()
+                require("mason-lspconfig").setup({
+                    automatic_installation = true,
+                })
+            end,
         },
         config = function()
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
