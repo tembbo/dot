473489efc2bab5d5076e89cec61961ec9a9844a8
tembbo <m@tembbo.com>
2024-08-19 17:11:54 +0200

    feat(lsp): add diagnostic symbols

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -29,6 +29,7 @@ return {
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
             local lspconfig = require("lspconfig")
+
             local servers = {
                 "gopls",
                 "lua_ls",
@@ -43,6 +44,17 @@ return {
                 lspconfig[lsp].setup({
                     capabilities = capabilities,
                 })
+            end
+
+            local signs = {
+                { name = "DiagnosticSignError", text = " " },
+                { name = "DiagnosticSignWarn", text = " " },
+                { name = "DiagnosticSignHint", text = "󰘥 " },
+                { name = "DiagnosticSignInfo", text = " " },
+            }
+
+            for _, sign in ipairs(signs) do
+                vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
             end
 
             vim.keymap.set("n", "A", vim.lsp.buf.code_action)
