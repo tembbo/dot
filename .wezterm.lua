8ebdfd865ef9f9617445b22984b41e71f0ecda02
tembbo <m@tembbo.com>
2024-09-13 21:19:21 +0200

    feat: use wezterm instead of kitty

---
+++ .wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require("wezterm")
+
+return {
+	color_scheme = "Catppuccin Mocha",
+	enable_tab_bar = false,
+
+	font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium" }),
+}
