9be4bc5d3a30338e132e78b4e9dc70356912692c
tembbo <m@tembbo.com>
2024-10-13 22:37:59 +0200

    feat!: switch to kitty again

--- .wezterm.lua
+++
@@ -1,9 +0,0 @@
-local wezterm = require("wezterm")
-
-return {
-	color_scheme = "Catppuccin Mocha",
-	enable_tab_bar = false,
-	window_close_confirmation = "NeverPrompt",
-
-	font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium" }),
-}
