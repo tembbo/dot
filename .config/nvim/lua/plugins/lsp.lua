cef0b90a7bcd8b4054281da96af1d8ab23a16e51
tembbo <m@tembbo.com>
2024-11-01 16:19:26 +0100

    refactor: switch to `opts`

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -19,11 +19,9 @@ return {
         event = { "BufReadPre", "BufNewFile" },
         dependencies = {
             "williamboman/mason-lspconfig.nvim",
-            config = function()
-                require("mason-lspconfig").setup({
-                    automatic_installation = true,
-                })
-            end,
+            opts = {
+                automatic_installation = true,
+            },
         },
         config = function()
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
