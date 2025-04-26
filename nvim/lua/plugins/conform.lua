50417cf39352e11039e3e34360429637a27e1877
tembbo <m@tembbo.com>
2025-04-26 11:56:32 +0200

    fix: increase timeout limit

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -21,7 +21,7 @@ return {
             rust = { "rustfmt" },
         },
         format_on_save = {
-            timeout_ms = 500,
+            timeout_ms = 1000,
             lsp_format = "fallback",
         },
     },
