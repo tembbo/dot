ec1a25a632b83f31a718ace7df68b630f1abc728
tembbo <m@tembbo.com>
2025-07-28 17:49:59 +0200

    chore(nvim): update mason on build

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,6 +2,7 @@ return {
     {
         "williamboman/mason.nvim",
         event = "VeryLazy",
+        build = ":MasonUpdate",
         opts = {
             ui = {
                 border = "rounded",
