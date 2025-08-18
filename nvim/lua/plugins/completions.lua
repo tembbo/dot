494bad386eb9b80fc7bfa08f2e53f352d3b839ee
tembbo <m@tembbo.com>
2025-08-18 21:56:57 +0200

    feat(nvim): overhaul completions

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -4,12 +4,16 @@ return {
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
         "rafamadriz/friendly-snippets",
+        "onsails/lspkind.nvim",
     },
     config = function()
         local cmp = require("cmp")
+        local lspkind = require("lspkind")
+
         require("luasnip.loaders.from_vscode").lazy_load()
 
         cmp.setup({
@@ -22,14 +26,22 @@ return {
                 completion = cmp.config.window.bordered(),
                 documentation = cmp.config.window.bordered(),
             },
+            formatting = {
+                format = lspkind.cmp_format({
+                    maxwidth = 30,
+                    ellipsis_char = "...",
+                }),
+            },
             mapping = cmp.mapping.preset.insert({
+                ["<c-k>"] = cmp.mapping.select_prev_item(),
+                ["<c-j>"] = cmp.mapping.select_next_item(),
                 ["<cr>"] = cmp.mapping.confirm({ select = true }),
             }),
             sources = cmp.config.sources({
                 { name = "nvim_lsp" },
                 { name = "luasnip" },
-            }, {
                 { name = "buffer" },
+                { name = "path" },
             }),
         })
     end,
