f1dc51d90f3e53ef792e9afc53e117915bb7987b
tembbo <m@tembbo.com>
2025-08-18 21:57:19 +0200

    feat(lsp): overhaul lsp stuff

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,49 +1,64 @@
 return {
     {
-        "williamboman/mason.nvim",
-        event = "VeryLazy",
+        "mason-org/mason.nvim",
         build = ":MasonUpdate",
-        opts = {
-            ui = {
-                border = "rounded",
-                backdrop = 100,
-                height = 0.8,
-                icons = {
-                    package_installed = "󰄬",
-                    package_pending = "󰁔",
-                    package_uninstalled = "󰅖",
-                },
-            },
+        dependencies = {
+            "mason-org/mason-lspconfig.nvim",
         },
+        config = function()
+            local mason = require("mason")
+            local mason_lspconfig = require("mason-lspconfig")
+
+            mason.setup({
+                ui = {
+                    border = "rounded",
+                    backdrop = 100,
+                    height = 0.8,
+                    icons = {
+                        package_installed = "󰄬",
+                        package_pending = "󰁔",
+                        package_uninstalled = "󰅖",
+                    },
+                },
+            })
+
+            mason_lspconfig.setup({
+                ensure_installed = {
+                    "cssls",
+                    "lua_ls",
+                    "ruff",
+                    "rust_analyzer",
+                    "svelte",
+                    "tailwindcss",
+                    "ts_ls",
+                },
+
+                automatic_installation = true,
+            })
+        end,
     },
     {
         "neovim/nvim-lspconfig",
         event = { "BufReadPre", "BufNewFile" },
         dependencies = {
             "hrsh7th/cmp-nvim-lsp",
-            "williamboman/mason-lspconfig.nvim",
+            "mason-org/mason-lspconfig.nvim",
         },
         config = function()
-            local lspconfig = require("lspconfig")
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            local servers = {
-                "cssls",
-                "denols",
-                "lua_ls",
-                "ruff",
-                "rust_analyzer",
-                "svelte",
-                "tailwindcss",
-            }
-
-            for _, lsp in ipairs(servers) do
-                lspconfig[lsp].setup({
-                    capabilities = capabilities,
-                })
-            end
+            vim.api.nvim_create_autocmd("LspAttach", {
+                group = vim.api.nvim_create_augroup("UserLspconfig", {}),
+                callback = function()
+                    vim.keymap.set("n", "A", vim.lsp.buf.code_action)
+                    vim.keymap.set("n", "D", vim.lsp.buf.definition)
+                    vim.keymap.set("n", "R", vim.lsp.buf.references)
+                    vim.keymap.set("n", "H", vim.lsp.buf.hover)
+                end,
+            })
 
             vim.diagnostic.config({
+                virtual_text = true,
                 signs = {
                     text = {
                         [vim.diagnostic.severity.ERROR] = " ",
@@ -51,14 +66,31 @@ return {
                         [vim.diagnostic.severity.INFO] = " ",
                         [vim.diagnostic.severity.HINT] = " ",
                     },
-                    numhl = {},
+                },
+                underline = true,
+                update_in_insert = false,
+                severity_sort = true,
+                float = {
+                    border = "rounded",
+                    source = "always",
+                    header = "",
+                    prefix = "",
                 },
             })
 
-            vim.keymap.set("n", "A", vim.lsp.buf.code_action)
-            vim.keymap.set("n", "D", vim.lsp.buf.definition)
-            vim.keymap.set("n", "R", vim.lsp.buf.references)
-            vim.keymap.set("n", "H", vim.lsp.buf.hover)
+            vim.lsp.config("*", {
+                capabilities = capabilities,
+            })
+
+            vim.lsp.config("lua_ls", {
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { "vim" },
+                        },
+                    },
+                },
+            })
         end,
     },
 }
