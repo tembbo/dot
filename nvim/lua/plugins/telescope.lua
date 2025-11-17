d37939a834c0238e4bc67eda57d062d67ba90e14
tembbo <m@tembbo.com>
2025-11-17 16:45:27 +0100

    feat(nvim): improve telescope config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,10 +1,27 @@
 return {
 	{
 		"nvim-telescope/telescope.nvim",
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+			"nvim-telescope/telescope-ui-select.nvim",
+		},
 		keys = {
 			{ "<leader>f", "<cmd>Telescope find_files<cr>" },
 			{ "<leader>g", "<cmd>Telescope live_grep<cr>" },
 		},
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					fzf = {},
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+
+			require("telescope").load_extension("fzf")
+			require("telescope").load_extension("ui-select")
+		end,
 	},
 }
