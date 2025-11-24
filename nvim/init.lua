b96e17710da5d0647a52d013bc6d476e626a0bad
tembbo <m@tembbo.com>
2025-11-24 22:13:29 +0100

    chore(nvim): remove todo comments plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -402,15 +402,6 @@ require("lazy").setup({
 			vim.cmd.colorscheme("catppuccin")
 		end,
 	},
-
-	-- Highlight todo, notes, etc in comments
-	{
-		"folke/todo-comments.nvim",
-		event = "VimEnter",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = { signs = false },
-	},
-
 	{ -- Collection of various small independent plugins/modules
 		"echasnovski/mini.nvim",
 		config = function()
