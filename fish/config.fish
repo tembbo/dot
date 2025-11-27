892c76f652fb1cd947be153f2627c7627b967ed7
tembbo <m@tembbo.com>
2025-11-27 21:57:16 +0100

    style(fish): reorder config statements

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,7 @@
 set fish_greeting
 
+set -gx EDITOR (which nvim)
+
 alias ls "eza --group-directories-first"
 alias la "eza --group-directories-first --all"
-
 alias vim nvim
-
-set -gx EDITOR (which nvim)
