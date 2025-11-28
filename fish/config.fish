a644af14790ea4c4baa9e0eb64fddf4400c0bbbc
tembbo <m@tembbo.com>
2025-11-28 23:48:08 +0100

    feat(fish): attach tmux on startup

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,14 @@ set -gx EDITOR (which nvim)
 alias ls "eza --group-directories-first"
 alias la "eza --group-directories-first --all"
 alias vim nvim
+
+if status is-interactive
+	and not set -q TMUX
+
+	if tmux has-session -t dev 2>/dev/null
+		tmux attach-session -t dev
+	else
+		tmux new-session -s dev
+		tmux attach-session -t dev
+	end
+end
