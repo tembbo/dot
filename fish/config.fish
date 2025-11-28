8bb0544987e94d828aa976d05c8e6fdfc86cf081
tembbo <m@tembbo.com>
2025-11-28 23:49:27 +0100

    refactor(fish): simplify tmux logic

--- fish/config.fish
+++ fish/config.fish
@@ -8,11 +8,5 @@ alias vim nvim
 
 if status is-interactive
 	and not set -q TMUX
-
-	if tmux has-session -t dev 2>/dev/null
-		tmux attach-session -t dev
-	else
-		tmux new-session -s dev
-		tmux attach-session -t dev
-	end
+	tmux new-session -A -s dev
 end
