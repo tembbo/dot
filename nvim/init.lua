5835ea9f768ff34ee71a2e468d9952cbfa7d3144
tembbo <m@tembbo.com>
2025-11-30 21:23:55 +0100

    feat(nvim): improve options

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,40 +1,27 @@
 vim.g.mapleader = " "
-vim.g.maplocalleader = " "
 
-vim.o.number = true
-vim.o.mouse = "a"
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+vim.opt.breakindent = true
 
-vim.o.showmode = false
+vim.opt.cursorline = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 10
+vim.opt.showmode = false
+vim.opt.signcolumn = "yes"
+vim.opt.title = true
+vim.opt.wrap = false
 
-vim.schedule(function()
-	vim.o.clipboard = "unnamedplus"
-end)
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = false
-vim.o.breakindent = true
-
-vim.o.undofile = true
-
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
-vim.o.signcolumn = "yes"
-
-vim.o.updatetime = 250
-
-vim.o.timeoutlen = 300
-
-vim.o.wrap = false
-
-vim.o.inccommand = "split"
-
-vim.o.cursorline = true
-
-vim.o.scrolloff = 10
-
-vim.o.confirm = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.confirm = true
+vim.opt.undofile = true
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
 
 vim.keymap.set("n", "<esc>", "<cmd>nohlsearch<cr>")
 
