e0289b71c3f614e3f8ef434bfde16158b20aa7a3
tembbo <m@tembbo.com>
2025-12-01 21:02:53 +0100

    refactor(nvim): deconstruct lazy plugins

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,22 @@
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+vim.opt.breakindent = true
+
+vim.opt.cursorline = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 10
+vim.opt.showmode = false
+vim.opt.signcolumn = "yes"
+vim.opt.title = true
+vim.opt.wrap = false
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.clipboard = "unnamedplus"
+vim.opt.confirm = true
+vim.opt.undofile = true
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,11 @@
+return {
+	"stevearc/oil.nvim",
+	keys = {
+		{ "-", "<cmd>Oil --float<cr>" },
+	},
+	opts = {
+		view_options = {
+			show_hidden = true,
+		},
+	},
+}
