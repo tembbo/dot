7b764bffb2944b6f56c928039f11f089b8f2ab11
tembbo <m@tembbo.com>
2025-12-03 21:16:42 +0100

    chore(lazy): move lazy setup to init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,28 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<esc>", "<cmd>nohlsearch<cr>")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
 
-require("config.lazy")
 require("config.options")
+
+require("lazy").setup({
+	spec = {
+		{ import = "plugins" },
+	},
+	change_detection = {
+		notify = false,
+	},
+})
