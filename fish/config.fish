5b6bc27b885fd646ae9cbec6e14c4b69b2f2fc99
tembbo <m@tembbo.com>
2025-12-07 20:39:11 +0100

    feat(fish): don't attach to tmux on startup

--- fish/config.fish
+++ fish/config.fish
@@ -5,8 +5,3 @@ set -gx EDITOR (which nvim)
 alias ls "eza --group-directories-first"
 alias la "eza --group-directories-first --all"
 alias vim nvim
-
-if status is-interactive
-	and not set -q TMUX
-	tmux new-session -A -s dev
-end
