04d1846d5d918a26cba4dbc0a1379b2f39386738
tembbo <m@tembbo.com>
2025-12-11 22:29:44 +0100

    attach tmux on startup

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,8 @@ set -gx EDITOR (which nvim)
 alias ls "eza --group-directories-first"
 alias la "eza --group-directories-first --all"
 alias vim nvim
+
+if status is-interactive
+    and not set -q TMUX
+    tmux new-session -A -s dev
+end
