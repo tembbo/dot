cc0214b60789ffa45a54ff2b45980848473d65f4
tembbo <m@tembbo.com>
2025-12-29 23:13:14 +0100

    enable granular control over path

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,7 @@ set -gx EDITOR (which nvim)
 alias ls "eza --group-directories-first"
 alias la "eza --group-directories-first --all"
 alias vim nvim
+
+set -e fish_user_paths
+fish_add_path /opt/homebrew/bin
+fish_add_path ~/.bun/bin
