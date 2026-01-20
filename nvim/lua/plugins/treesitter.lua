241bcc9b2c34fa7ab595efa3882cbabea06aa483
tembbo <m@tembbo.com>
2026-01-20 21:53:14 +0100

    revert treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,28 +1,33 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPre", "BufNewFile" },
-	build = ":TSUpdate",
-	opts = {
-		ensure_installed = {
-			"bash",
-			"css",
-			"html",
-			"javascript",
-			"json",
-			"lua",
-			"luadoc",
-			"markdown",
-			"markdown_inline",
-			"svelte",
-			"vimdoc",
-			"zig",
-		},
-		auto_install = true,
-		highlight = {
-			enable = true,
-		},
-		indent = {
-			enable = true,
-		},
-	},
+	branch = "master",
+	event = { "BufEnter" },
+	build = function()
+		require("nvim-treesitter.install").update({ with_sync = true })
+	end,
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"bash",
+				"css",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"luadoc",
+				"markdown",
+				"markdown_inline",
+				"svelte",
+				"vimdoc",
+				"zig",
+			},
+			auto_install = true,
+			highlight = {
+				enable = true,
+			},
+			indent = {
+				enable = true,
+			},
+		})
+	end,
 }
