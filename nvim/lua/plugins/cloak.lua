c0913028be0c366b33b8a3ca2fb82c571fd2c2cd
tembbo <m@tembbo.com>
2026-03-21 22:27:40 +0100

    cloack

--- nvim/lua/plugins/cloak.lua
+++ nvim/lua/plugins/cloak.lua
@@ -1,3 +1,29 @@
 return {
-	"laytan/cloak.nvim",
+	{
+		"laytan/cloak.nvim",
+		config = function()
+			require("cloak").setup({
+				patterns = {
+					{
+						file_pattern = "**/*.vars*",
+						cloak_pattern = "=.+",
+					},
+					{
+						file_pattern = "**/*.env*",
+						cloak_pattern = "=.+",
+					},
+					{
+						file_pattern = "**/*.opencode.json",
+						cloak_pattern = '("apiKey":) .+',
+						replace = "%1 ",
+					},
+					{
+						file_pattern = "**/config.toml",
+						cloak_pattern = "(token =) .+",
+						replace = "%1 ",
+					},
+				},
+			})
+		end,
+	},
 }
